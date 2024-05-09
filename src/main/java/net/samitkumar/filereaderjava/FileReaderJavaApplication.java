e3749f1021029f54adc9b8980c460b1305209912
samitkumarpatel <samitkumarpatel@gmail.com>
2024-05-10 00:39:31 +0200

    first commit

---
+++ src/main/java/net/samitkumar/filereaderjava/FileReaderJavaApplication.java
@@ -0,0 +1,13 @@
+package net.samitkumar.filereaderjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileReaderJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileReaderJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/samitkumar/filereaderjava/FileReaderJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.samitkumar.filereaderjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileReaderJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
